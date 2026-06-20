223f4644df4251f73838b043e29703d7838e95cc
YiHong <leeyihong12.com>
2026-06-20 11:28:52 +0800

    Minor updates

--- Receipt-Expense-Tracker/src/text_extractor.py
+++ Receipt-Expense-Tracker/src/text_extractor.py
@@ -1,11 +1,17 @@
 import keras_ocr
 
+# Keras OCR from:https://pypi.org/project/keras-ocr/
+print("[SYSTEM] Booting up Keras-OCR Engine. Please wait...")
+pipeline = keras_ocr.pipeline.Pipeline()
+print("[SYSTEM] OCR Engine is ready!")
+
 def extract_raw_text(image_path):
-    pipeline = keras_ocr.pipeline.Pipeline()
     images = [keras_ocr.tools.read(image_path)]
     preds = pipeline.recognize(images)[0]
 
     # Sort by top to bottom, then left to right (natural reading order)
     preds.sort(key=lambda x: (x[1][0][1], x[1][0][0]))
     
-    return [word for word, _ in preds]
+    extracted_words = [word for word, _ in preds]
+
+    return extracted_words
